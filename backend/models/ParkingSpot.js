0f82609deb73ef0788848981d20b1be20f28749f
Ida <imgjeits@stud.ntnu.no>
2021-10-19 15:30:41 +0200

    set default value for availability property

--- backend/models/ParkingSpot.js
+++ backend/models/ParkingSpot.js
@@ -1,21 +1,25 @@
 const mongoose = require('mongoose')
 
-const ParkingSpot = mongoose.Schema({
-	area: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'ParkingArea',
-		required: true
+const ParkingSpot = mongoose.Schema(
+	{
+		area: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'ParkingArea',
+			required: true
+		},
+		type: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'ParkingType',
+			required: true
+		},
+		availability: {
+			type: Boolean,
+			required: true,
+			default: true
+		}
 	},
-	type: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'ParkingType',
-		required: true
-	},
-	availability: {
-		type: Boolean,
-		required: true
-	}
-}, { collection: 'parking-spots' })
+	{ collection: 'parking-spots' }
+)
 
 // Export
 module.exports = mongoose.model('ParkingSpot', ParkingSpot)
