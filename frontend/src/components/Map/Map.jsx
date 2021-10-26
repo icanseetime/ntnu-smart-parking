78a76b0672a8bda6d47f93c81431fe75c468a72d
Ida <imgjeits@stud.ntnu.no>
2021-10-26 19:23:37 +0200

    Map component

---
+++ frontend/src/components/Map/Map.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Map() {
+	return (
+		<div
+			style={{
+				backgroundImage: 'url(./campus-map.jpg)',
+				height: '60vh',
+				backgroundPosition: 'top right',
+				backgroundSize: 'cover'
+			}}
+		></div>
+	)
+}
