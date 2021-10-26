29d18b20c43aac3e551321481a6d39a74ec482e1
Ida <imgjeits@stud.ntnu.no>
2021-10-26 14:13:34 +0200

    added route to list all parking areas

--- backend/routes/parking-areas/index.js
+++ backend/routes/parking-areas/index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const router = express.Router()
 const routes = require('./routes')
 
+// GET: List all parking areas
+router.get('/', routes.listParkingAreas)
+
 // POST: Create new parking area
 router.post('/', routes.createParkingAreas)
 
--- backend/routes/parking-areas/routes.js
+++ backend/routes/parking-areas/routes.js
@@ -1,24 +1,43 @@
 // Database schemas
 const ParkingArea = require('../../models/ParkingArea')
 
+// GET: List all parking areas
+const listParkingAreas = async (req, res) => {
+	try {
+		const list = await ParkingArea.find(req.query)
+		if (!list) {
+			res.status(404).json({
+				error: `Couldn't find any parking areas in the database.`
+			})
+		} else {
+			res.status(200).json(list)
+		}
+	} catch (err) {
+		res.status(500).json({
+			error: `Something went wrong while trying to list the parking areas. [${err}]`
+		})
+	}
+}
+
 // POST: Create new parking area
 const createParkingAreas = async (req, res) => {
-    try {
-        const newParkingArea = new ParkingArea({
-            name: req.body.name
-        })
-        const parkingArea = await newParkingArea.save()
-        res.status(201).json({
-            message: `New parking area successfully created!`,
-            parkingArea 
-        })
-    } catch (err) {
-        res.status(500).json({
-            error: `Something went wrong while trying to create a new parking area. [${err}]`
-        })
-    }
+	try {
+		const newParkingArea = new ParkingArea({
+			name: req.body.name
+		})
+		const parkingArea = await newParkingArea.save()
+		res.status(201).json({
+			message: `New parking area successfully created!`,
+			parkingArea
+		})
+	} catch (err) {
+		res.status(500).json({
+			error: `Something went wrong while trying to create a new parking area. [${err}]`
+		})
+	}
 }
 
 module.exports = {
-    createParkingAreas
+	listParkingAreas,
+	createParkingAreas
 }
