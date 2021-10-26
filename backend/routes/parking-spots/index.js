1ead3d1f33ecbeb065cecc2c7ecf251b11e351a6
Ida <imgjeits@stud.ntnu.no>
2021-10-26 19:22:55 +0200

    added route to list parking spots in specified area

--- backend/routes/parking-spots/index.js
+++ backend/routes/parking-spots/index.js
@@ -9,4 +9,7 @@ router.get('/', routes.listParkingSpots)
 // POST: Create new parking spot
 router.post('/', routes.createParkingSpot)
 
+// GET: List available parking spots in selected area, split by type
+router.get('/:area', routes.listAvailabilityInArea)
+
 module.exports = router
--- backend/routes/parking-spots/routes.js
+++ backend/routes/parking-spots/routes.js
@@ -41,7 +41,38 @@ const createParkingSpot = async (req, res) => {
 	}
 }
 
+const listAvailabilityInArea = async (req, res) => {
+	try {
+		// Check for area missing
+		if (!req.params.area) {
+			return res.status(400).json({
+				error: `You need to include the ID of an area in your request.`
+			})
+		}
+
+		// Find all parking spots in area
+		const list = await ParkingSpot.find({
+			area: req.params.area
+		}).populate('type')
+
+		// Check for missing parking spots in DB
+		if (!list.length) {
+			return res.status(404).json({
+				error: `Couldn't find any parking spots in the selected area / parking spot type.`
+			})
+		}
+
+		// Send results to client
+		res.status(200).json(list)
+	} catch (err) {
+		res.status(500).json({
+			error: `Something went wrong while trying find parking spots in the selected area. [${err}]`
+		})
+	}
+}
+
 module.exports = {
 	listParkingSpots,
-	createParkingSpot
+	createParkingSpot,
+	listAvailabilityInArea
 }
