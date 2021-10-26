77a40c1cc941e1995923f6a291edada6f4d6d65e
Ida <imgjeits@stud.ntnu.no>
2021-10-26 19:22:32 +0200

    setup proxy

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,9 @@
+if (process.env.NODE_ENV !== 'production') {
+	const { createProxyMiddleware } = require('http-proxy-middleware')
+
+	module.exports = function (app) {
+		app.use(
+			createProxyMiddleware('/api', { target: 'http://localhost:3001' })
+		)
+	}
+}
