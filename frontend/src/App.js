b0f2b91461742acba064eaab4e559bcd1e461bed
Ida <imgjeits@stud.ntnu.no>
2021-10-26 19:25:13 +0200

    Add all components to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import './App.css'
 
+// Components
+import Header from './components/Header/Header'
+import LocationsAccordion from './components/LocationsAccordion/LocationsAccordion'
+import Map from './components/Map/Map'
+
 function App() {
-	return <div className="App"></div>
+	return (
+		<div className="App">
+			<Header />
+			<Map />
+			<LocationsAccordion />
+		</div>
+	)
 }
 
 export default App
